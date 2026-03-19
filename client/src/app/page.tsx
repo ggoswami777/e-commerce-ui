f96f6df5b6dfcb2926f1e91344e0a1925da4ec5e
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-03-19 21:05:51 +0530

    added categories and landing image

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,15 @@
+import ProductList from "@/components/ProductList"
+import Image from "next/image"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Feature Product" fill/>
+      </div>
+      
+      <ProductList/>
+    </div>
   )
 }
 
