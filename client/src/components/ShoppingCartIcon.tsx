2f4941c3042d3a7455ea1bd695e6dcde3ce5c2e1
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-03-30 19:29:43 +0530

    added zustand and store

--- client/src/components/ShoppingCartIcon.tsx
+++ client/src/components/ShoppingCartIcon.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import useCartStore from "@/stores/cartStore"
 import { ShoppingCart } from "lucide-react"
 import Link from "next/link"
 
 const ShoppingCartIcon = () => {
+  const {cart}=useCartStore();
   return (
-    <Link href="/cart">
-        <ShoppingCart className='w-4 h-4 text-gray-500 '/>
-        <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-sm font-medium">0</span>
+    <Link href="/cart" className="relative">
+        <ShoppingCart className='w-4 h-4 text-gray-500 ' />
+        <span className="absolute -top-2 -right-2 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-[10px] font-medium leading-none">
+          {cart.length}
+        </span>
     </Link>
+
   )
 }
 
